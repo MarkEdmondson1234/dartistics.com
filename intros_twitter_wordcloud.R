97452ae120e7d2523261f2ab6359967bc1bbce92
gilliganondata <tim@analyticsdemystified.com>
2016-08-15 20:58:21 -0400

    Found the issue with my OAuth on twitteR (I don't want to talk about it). So...back to seeing if I can make a little "word cloud of all the tweets" as a way to do the class intros.

--- intros_twitter_wordcloud.R
+++ intros_twitter_wordcloud.R
@@ -1,22 +1,36 @@
 # The idea was to get the Twitter handle for every student as part of the introductions.
 # Add those to a .csv that is just open in RSTudio. Then, run a script that pulls the
 # last 30 days of tweets from all of the students and generates a word cloud.
-# But... 1.5 hours in, and I can't get twitteR to authenticate.
-
 
 library(twitteR)
 library(dplyr)
 
 # Authorize Twitter
 consumerKey <- "PBZ1OOC61Fa4Rvu00weipJcHH"
-consumerSecret <- "m6OlgtM0iWpKUbnsMrPG4pv2NJC6BJVcZhXpUsImFnOCTaK6yf1"
+consumerSecret <- "m6OlgtM0iWpKUbnsMrPG4pv2NJC6BJVcZhXpUsImFnOCTaK6yf"
 accessToken <- "9459132-AQbfbaDUhMvPlQNzjgcIXJWhGmnb22sJ9jckyctyUG"
 accessTokenSecret <- "FvGseA55680hAOEgHWyMCSynF9GVvm49a97nquOLMqLkv"
 setup_twitter_oauth(consumerKey,consumerSecret,accessToken,accessTokenSecret)
 
+# Get the list of usernames
+usernames <- read.csv("intros_twitter_wordcloud_usernames.csv", stringsAsFactors = FALSE)
 
-# Curses!!! CANNOT get this to run without generating the following:
-#     Error in check_twitter_oauth() : OAuth authentication error:
-#     This most likely means that you have incorrectly called setup_twitter_oauth()'
-# Lots of StackExchange chatter on it, but none of the fixes seem to work.
-# Seems to be something Mac-related, I think.
+# Set the start date as the last 30 days
+startDate <- as.character(Sys.Date()-30)
+
+# Initialize a data frame that we'll append tweets to. We only want the 
+# username and the tweet.
+tweetData <- data.frame(username = character(),tweet = character())
+
+# Pull the tweets for each username and put them in an overall data frame
+for (i in 1:nrow(usernames)){
+  # pull the list of tweets and put them in a data frame
+  userTweets <- twListToDF(searchTwitter(paste('from:',
+                                               usernames$username[i],"'",sep=""),
+                                         n = 1e4, since = startDate))
+  
+  # append these tweets to the master data set
+  tweetData <- rbind(tweetData,data.frame(username = userTweets$screenName, tweet = userTweets$text))
+}
+
+# Make a word cloud from the tweets
