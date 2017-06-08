e4cf580eb45f9a06a739681cd586dcbf359eb81c
gilliganondata <tim@analyticsdemystified.com>
2017-06-07 21:55:58 -0400

    Added intro slides

--- intro_slides/ggplot_themes.R
+++ intro_slides/ggplot_themes.R
@@ -24,11 +24,11 @@ theme_bar <- default_theme +
   theme(panel.grid.major.y = element_blank(),
         axis.text.y = element_blank(),
         axis.text.x = element_text(face = "bold", size = 20, colour = "grey5",
-                                   margin=margin(10,0,0,0, "pt")))
+                                   margin = margin(10,0,0,0, "pt")))
 
 theme_bar_horiz <- default_theme +
   theme(axis.text.y = element_text(face = "plain", size = 12, colour = "grey5",
-                                   margin=margin(10,0,0,0, "pt")),
+                                   margin = margin(10,0,0,0, "pt")),
         axis.line.x = element_blank(),
         axis.line.y = element_line(colour = "grey30"),
         axis.text.x = element_blank(),
@@ -37,9 +37,10 @@ theme_bar_horiz <- default_theme +
 
 theme_heatmap <- default_theme +
   theme(panel.grid.major.y = element_blank(),
-        axis.text = element_text(face = "plain", size = 15, colour = "black"),
-        axis.title = element_text(face = "bold", size = 20, colour = "black",
-                                  margin=margin(5,5,5,5, "pt"),
-                                  vjust = -2),
+        axis.text.x = element_text(face = "plain", size = 15, colour = "black",
+                                 margin = margin(0,0,10,0)),
+        axis.text.y = element_text(face = "plain", size = 15, colour = "black",
+                                   margin = margin(0,0,0,10)),
+        axis.title = element_text(face = "bold", size = 20, colour = "black"),
         axis.line.x = element_blank(),
         legend.position = "none")
